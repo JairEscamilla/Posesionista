019b66a0d76c51f4395edabb1da244abd5e83866
JairEscamilla <jair@kalyptio.com>
2021-10-14 19:15:32 -0500

    Adding format to the date

--- app/src/main/java/com/example/posesionista/Cosa.kt
+++ app/src/main/java/com/example/posesionista/Cosa.kt
@@ -2,19 +2,23 @@ package com.example.posesionista
 
 import android.os.Parcel
 import android.os.Parcelable
+import java.text.SimpleDateFormat
 import java.util.*
 
 class Cosa(): Parcelable{
+    private val simpleDateFormat = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
     var nombreDeCosa: String = ""
     var valorEnPesos: Int = 0
     var numeroDeSerie: String = UUID.randomUUID().toString().substring(0, 6)
-    var fechaDeCreacion: Date = Date()
+    var fechaDeCreacion: String = simpleDateFormat.format(Date())
+
+
 
     constructor(parcel: Parcel) : this(){
         nombreDeCosa = parcel.readString().toString()
         valorEnPesos = parcel.readInt()
         numeroDeSerie = parcel.readString().toString()
-        fechaDeCreacion = parcel.readSerializable() as Date
+        fechaDeCreacion = parcel.readString().toString()
     }
 
     override fun describeContents(): Int {
@@ -25,7 +29,7 @@ class Cosa(): Parcelable{
         dest.writeString(nombreDeCosa)
         dest.writeInt(valorEnPesos)
         dest.writeString(numeroDeSerie)
-        dest.writeSerializable(fechaDeCreacion)
+        dest.writeString(fechaDeCreacion)
     }
 
     companion object CREATOR : Parcelable.Creator<Cosa> {
